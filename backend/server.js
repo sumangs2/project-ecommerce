50e07dec6f201e46ced9d196af4a86c0bcf35e46
juan lorenzo sumangil <juanlorenzo.sumangil2@gmail.com>
2022-03-26 21:05:12 +0800

    added backend server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from "express";
-
+import data from "./data.js";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Server is ready"); //res = response
+  res.send("Server is ready");
+});
+
+app._router.get("/api/products", (req, res) => {
+  res.send(data.products);
 });
 
 const port = process.env.PORT || 4000;
